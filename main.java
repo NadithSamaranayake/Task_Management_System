1be375d6ee6e04bd2a54af5b7d84ae47ee8a5dac
Nadith Samaranayake <nadith.jsamaranayake@gmail.com>
2024-02-19 10:34:12 +0530

    commit1

---
+++ main.java
@@ -0,0 +1,24 @@
+class Node
+{
+    int priority; //priority of the task
+    double excTime; //execution time
+    String tName; //task Name
+    Node prev; //previous node
+    Node next; //next node
+}
+class LinkedList
+{
+    Node head;
+    public LinkedList()
+    {
+        head = null;
+    }
+    public void insert(int )
+}
+public class main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
